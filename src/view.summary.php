552a453454f199fa6a023d0e85d73d5ffbebd768
Mikael Svensson <mikael.svensson@klarna.com>
2017-11-12 16:55:49 +0100

    List top teams

--- src/view.summary.php
+++ src/view.summary.php
@@ -5,12 +5,15 @@ function tsl_print_overview_report($competition_name)
     $teams = tsl_get_team_list();
 
     $age_group_top_score = array();
+    $top_of_age_group = array();
     foreach ($teams as $team) {
         $score = tsl_grade_final($competition_name, $team->team_name);
         $team->score = $score;
         if ($age_group_top_score[$team->age_group] < $score) {
             $age_group_top_score[$team->age_group] = $score;
         }
+
+        $top_of_age_group[$team->age_group][$score][] = $team->team_name;
     }
 
     usort($teams, function ($a, $b) {
@@ -38,11 +41,33 @@ function tsl_print_overview_report($competition_name)
         $start_url,
         $competition_url,
         $competition_name);
-    printf('<table style="border-collapse: collapse;"><tbody>');
+
+    printf('<h2>Toppskiktet</h2>');
+
+    printf('<table style="border-collapse: collapse;" cellpadding="3"><tbody>');
+
+    foreach ($top_of_age_group as $age_group => $teams_per_score) {
+        krsort($teams_per_score);
+        printf('<tr><td colspan="2"><strong>Åldergrupp %s</strong></td></tr>', $age_group);
+        $count = 3;
+        foreach ($teams_per_score as $score => $teams_with_score) {
+            if ($score > 0) {
+                printf('<tr><td>%s poäng</td><td>%s</td></tr>', $score, join(", ", $teams_with_score));
+                if (--$count < 1) {
+                    break;
+                }
+            }
+        }
+    }
+
+    printf('</tbody></table>');
+
+    printf('<h2>Alla</h2>');
+    printf('<table style="border-collapse: collapse;" cellpadding="3"><tbody>');
 
     foreach ($teams as $team) {
         if ($last_age_group != $team->age_group) {
-            printf('<tr><td colspan="3">%s</td></tr>', $team->age_group);
+            printf('<tr><td colspan="3"><strong>Åldergrupp %s</strong></td></tr>', $team->age_group);
         }
         $url = add_query_arg(array(
             'tsl_report' => 'nextgen',
@@ -51,7 +76,7 @@ function tsl_print_overview_report($competition_name)
         printf('' .
             '<tr>' .
             '<td><a href="%s">%s</a></td>' .
-            '<td>%s</td>' .
+            '<td>%s p</td>' .
             '<td>%s</td>' .
             '</tr>',
             $url,
