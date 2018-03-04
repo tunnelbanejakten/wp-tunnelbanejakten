e019dd849c0a36da7a2d5ece964c93e1075010f2
Mikael Svensson <mikael.svensson@klarna.com>
2018-03-04 16:43:45 +0100

    Minor copy changes

--- src/view.start.php
+++ src/view.start.php
@@ -4,14 +4,14 @@ include_once 'sql/queries.php';
 
 function tsl_print_start_page()
 {
-    printf('<h1>TSL</h1>');
-    printf('<p>Välj tävling:</p>');
+    printf('<h1>Tunnelbanejakten</h1>');
+    printf('<p>Välj t&auml;vling:</p>');
     printf('<ul>%s</ul>', join(array_map(function ($name) {
         $url = add_query_arg(array(
             'tsl_report' => 'competition',
             'tsl_competition' => $name
         ));
-        return sprintf('<li><a href="%s">%s</a></li>', $url, $name);
+        return sprintf('<li style="font-size: 150%%;"><a href="%s">%s</a></li>', $url, $name);
     }, tsl_get_formgroups_list())));
     printf('<p><small>Den här listan baseras på vilka Formidable-formulär som heter tsl-<em>tävlingsnamn</em>-<em>delmoment</em>.</small></p>');
 }
